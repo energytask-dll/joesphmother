624c244b92f19cbd83e95bf8d1ad0a4362296b04
Zakriya <sqmapi@outlook.com>
2022-03-13 12:59:10 -0400

    i. jus wanna
    dieeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- components/Quote.js
+++ components/Quote.js
@@ -3,8 +3,8 @@ import Link from 'next/link'
 
 export default function Quote() {
     const quotes = [
-        ["do the fortnite!", "Ragarnoky"]
-        ["sussy balls", "Ragarnoky"]
+        ["do the fortnite!"],
+        ["sussy balls"]
     ]
     let random = quotes[Math.floor(Math.random()*quotes.length)]
     return (
