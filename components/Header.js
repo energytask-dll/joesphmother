1ff47d338c9e6635ec5929071f598b35168a3e83
Zakriya <sqmapi@outlook.com>
2022-03-13 12:00:37 -0400

    only house
    this is no longer noky property

--- components/Header.js
+++ components/Header.js
@@ -6,12 +6,6 @@ export default function Header() {
   return (
     <div>
       <Link href="/" ><a className={styles.defaultLinkButton}>🏠 Home</a></Link>
-      |
-      <Link href="roads"><a className={styles.defaultLinkButton}>🛣 Roadsigns</a></Link>
-      |
-      <Link href="https://ragarnoky.github.io/toastergiveaway/"><a className={styles.defaultLinkButton}>🍞 Toaster Giveaway</a></Link>
-      |
-      <Link href="https://uvrays.majdeeganexpwy.repl.co"><a className={styles.defaultLinkButton}>Alwakil</a></Link>
       <hr />
     </div>
   )
